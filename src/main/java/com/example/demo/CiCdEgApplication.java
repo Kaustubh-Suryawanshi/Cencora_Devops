013b81f8ea41d0ffd7f5968a23e3b848cf016422
Kaustubh-Suryawanshi <kaustubhks1956@gmail.com>
2024-09-04 12:00:42 +0000

    gitignore added

---
+++ src/main/java/com/example/demo/CiCdEgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdEgApplication.class, args);
+	}
+
+}
